db2fb3f6e19dd2e55c9ea4254da6428655ad590b
Mulkian <mulkianakhfa1@gmail.com>
2024-01-18 09:15:14 +0700

    Update cek_login.php

--- cek_login.php
+++ cek_login.php
@@ -9,12 +9,13 @@
  }
 
  if(isset($_SESSION['sid'])){
-    //variabel elemt session
+    //variabel element session
     $l_id = $_SESSION['sid'];
     $l_nama = $_SESSION['snama'];
     $l_email = $_SESSION['semail'];
+
  }elseif(isset($_COOKIE['cid'])){
-    // variabel elemnt cookie
+    // variabel element cookie
     $l_id = $_COOKIE['cid'];
     $l_nama = $_COOKIE['cnama'];
     $l_email = $_COOKIE['cemail'];
